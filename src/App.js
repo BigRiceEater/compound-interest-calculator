badeb5e4e4c0c225f253d939462f33a5d13e6a25
Big Rice Eater <34292181+BigRiceEater@users.noreply.github.com>
2023-04-01 20:15:37 +0800

    Add compound interest calculation when fields change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const TextField = (props) => (
   <Form.Group
@@ -26,23 +26,55 @@ const TextField = (props) => (
 );
 
 function App() {
-  const [principalValue, setPrincipalValue] = useState(0);
+  const [principalValue, setPrincipalValue] = useState(100);
+  const [interestRatePerAnnum, setInterestRatePerAnnum] = useState(0.003);
+  const [compoundFrequencyPerYear, setCompoundFrequencyPerYear] = useState(12);
+  const [depositTenorInYears, setDepositTenorInYears] = useState(1);
   const [futureValue, setFutureValue] = useState(0);
+  const [gainValue, setGainValue] = useState(0);
 
-  const calculateCompoundInterest = () => {};
+  useEffect(() => {
+    calculateCompoundInterest();
+  }, [
+    principalValue,
+    interestRatePerAnnum,
+    compoundFrequencyPerYear,
+    depositTenorInYears,
+  ]);
+
+  const calculateCompoundInterest = () => {
+    const result =
+      (principalValue * (1 + interestRatePerAnnum / compoundFrequencyPerYear)) ^
+      (compoundFrequencyPerYear * depositTenorInYears);
+    setFutureValue(result);
+    setGainValue(result - principalValue);
+  };
 
   return (
     <div className="App">
       <Form>
         <TextField
           label="Principal"
+          value={principalValue}
           onChange={(evt) => setPrincipalValue(evt.target.value)}
         />
-        <TextField label="Interest Rate Per Annum" />
-        <TextField label="Compound Frequency Per Year" />
-        <TextField label="Deposit Tenor in Years" />
-        <TextField label="Future Value" value={principalValue} readOnly />
-        <TextField label="Gain" readOnly />
+        <TextField
+          label="Interest Rate Per Annum"
+          value={interestRatePerAnnum}
+          onChange={(evt) => setInterestRatePerAnnum(evt.target.value)}
+        />
+        <TextField
+          label="Compound Frequency Per Year"
+          value={compoundFrequencyPerYear}
+          onChange={(evt) => setCompoundFrequencyPerYear(evt.target.value)}
+        />
+        <TextField
+          label="Deposit Tenor in Years"
+          value={depositTenorInYears}
+          onChange={(evt) => setDepositTenorInYears(evt.target.value)}
+        />
+        <TextField label="Future Value" value={futureValue} readOnly />
+        <TextField label="Gain" value={gainValue} readOnly />
       </Form>
     </div>
   );
